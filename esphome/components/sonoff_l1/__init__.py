b97c203bd13e1cbc4d0dd4bdfb2335e2fca2b524
tavarespl <frugakrzysztof@gmail.com>
2025-05-05 23:25:04 +0200

    Update __init__.py

--- esphome/components/sonoff_l1/__init__.py
+++ esphome/components/sonoff_l1/__init__.py
@@ -1,1 +1,11 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
 
+sonoff_l1_ns = cg.esphome_ns.namespace('sonoff_l1')
+SonoffL1 = sonoff_l1_ns.class_('SonoffL1', cg.Component)
+
+CONFIG_SCHEMA = cv.Schema({}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
